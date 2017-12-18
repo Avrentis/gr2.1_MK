25cdbaf645172d116da557f520f96a6729a0bfa3
Avrentis <avrentisgc@gmail.com>
2017-12-18 17:47:40 +0400

    Att2_prog_MK_gr.2.1

---
+++ 6.18/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tools;
+
+namespace _6._18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBtn_Click(object sender, EventArgs e)
+        {
+            string input = Input.Text;
+            ToolsStr str = new ToolsStr(input);
+            bool check = str.CheckOn('а', 'б');
+            if (check)
+                output.Text = "удволетворяет условию";
+            else
+                output.Text = "не удволетворяет условию";
+        }
+    }
+}
